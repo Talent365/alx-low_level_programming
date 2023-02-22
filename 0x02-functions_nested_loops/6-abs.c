4665408fa603ffce4a15c384e1fd731f2a8c6ec8
Talent365 <muchairitalent@gmail.com>
2023-02-21 18:25:07 -0800

    C - Functions, nested loops tasks

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - find absolute value of a number
+ * @num: number to be checked
+ *
+ * Description: find absolute
+ *
+ * Return: absolute of num
+ */
+
+int _abs(int num)
+{
+	return (num < 0 ? -num : num);
+}
+
