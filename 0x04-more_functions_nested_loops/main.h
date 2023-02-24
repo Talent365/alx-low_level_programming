89c2219d3e513114d5feccf3a016ff193cf6e006
Talent365 <muchairitalent@gmail.com>
2023-02-23 17:40:15 -0800

    C - More functions, more nested loops task

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+
+
+int _putchar(int);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+void print_triangle(int);
+void fizz_buzz(void);
+long prime_factor(void);
