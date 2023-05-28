7baca960908432f77c7621f6f85586392fc539a2
Talent365 <muchairitalent@gmail.com>
2023-05-28 08:27:10 -0700

    intro to C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main -use printf to print to screen
+ *
+ * Description: formatted printing
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
