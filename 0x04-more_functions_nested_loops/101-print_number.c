e557eb18faef3702987baf0fde052f3bcd0a0d1c
Talent365 <muchairitalent@gmail.com>
2023-06-06 13:30:41 -0700

    functions and nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,36 +1,46 @@
-#include "holberton.h"
-void print_integer(int m);
+#include "main.h"
 
 /**
- * print_number - a function that prints an integer.
- * @n: An input integer
- * Return: Nothing
+ * print_number - prints number
+ *
+ * @n: integer to print to character
  */
 void print_number(int n)
 {
+	int i;
+	int d = 1;
+	unsigned int x = n;
+	unsigned int y = n;
+	int c = 0;
+
 	if (n == 0)
+	{
 		_putchar('0');
-	else if (n < 0)
+	}
+	if (n < 0)
 	{
 		_putchar('-');
-		print_integer(n * -1);
+		n = n + 1;
+		n = -n;
+		y = n;
+		x = n;
+		x += 1;
+		y += 1;
 	}
-	else
-		print_integer(n);
-}
 
-/**
- * print_integer - A function to priting n
- * @m: an input unsigned integer
- * Return: Nothing
- */
-void print_integer(int m)
-{
-	int i = 1000000000;
-
-	for (; i >= 1; i /= 10)
-		if (m / i != 0)
-		{
-			_putchar((m / i) % 10 + '0');
-		}
+	while (x != 0)
+	{
+		x = x / 10;
+		c++;
+	}
+	for (i = 1; i < c; i++)
+	{
+		d *= 10;
+	}
+	for (i = 0; i < c; i++)
+	{
+		_putchar(y / d + '0');
+		y = y % d;
+		d = d / 10;
+	}
 }
